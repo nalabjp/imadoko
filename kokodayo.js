d356ea7bd62ae9602252908fafc26a72f2d1d301
nalabjp <nalabjp@gmail.com>
2015-06-17 02:41:27 +0900

    Create sample implementation

---
+++ kokodayo.js
@@ -0,0 +1,18 @@
+var noble = require('noble');
+
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    noble.startScanning();
+  } else {
+    noble.stopScanning();
+  }
+});
+
+noble.on('discover', function(peripheral) {
+    console.log('Found device with local name: ' + peripheral.advertisement.localName);
+    console.log('advertising the following service uuid\'s: ' + peripheral.advertisement.serviceUuids);
+    console.log();
+    peripheral.connect(function(error) {
+      console.log('connected to peripheral: ' + peripheral.uuid);
+    });
+});
